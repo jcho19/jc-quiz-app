0e9938be7d5a7f31c5fdc3753bfe3cd7fe8e972b
jcho19 <josephcho212@gmail.com>
2022-07-22 11:23:22 -0700

    revised server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000
+require('dotenv').config();
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {useNewUrlParser: true,
+    useUnifiedTopology: true});
+
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+dbConnect()
 
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-  })
+mongoose.connection.once('open', app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+}) )
+
+
