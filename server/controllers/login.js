6ee4e729220117ccd13de7ec36cf462ee836583e
jcho19 <josephcho212@gmail.com>
2022-07-24 20:36:51 -0700

    revised login.js in controllers #2

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -6,11 +6,11 @@ const handleLogin = async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username }).exec();
     if(!user) {
-        res.status(401).json({ 'message': 'Incorrect username or password.' });
+        return res.status(401).json({ 'message': 'Incorrect username or password.' });
     }
     const validPassword = await bcrypt.compare(password, user.password)
     if(!validPassword) {
-        res.status(401).json({ 'message': 'Incorrect username or password.' });
+        return res.status(401).json({ 'message': 'Incorrect username or password.' });
     }
     const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' });
     const refreshToken = jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
