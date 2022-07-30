bbb2a93710bf14d3e2b29cdc45d53713ea885f5f
jcho19 <josephcho212@gmail.com>
2022-07-30 15:04:22 -0700

    revised question.js in controllers #2

--- server/controllers/question.js
+++ server/controllers/question.js
@@ -1,8 +1,12 @@
 const Question = require('../models/question');
 
 const handleQuestion = async (req, res) => {
-    const questions = await Question.aggregate([{ $sample: { size: 12 } }]);
-    res.status(200).json(questions);
+    try {
+        const questions = await Question.aggregate([{ $sample: { size: 12 } }]);
+        res.status(200).json(questions);
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
 }
 
 module.exports = { handleQuestion }
