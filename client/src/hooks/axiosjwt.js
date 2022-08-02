534b00903e4614ffa316c45021b6236f842479b6
jcho19 <josephcho212@gmail.com>
2022-08-02 11:48:10 -0700

    revised axiosjwt.js #4

--- client/src/hooks/axiosjwt.js
+++ client/src/hooks/axiosjwt.js
@@ -4,6 +4,7 @@ import { TokenContext }from '../context/tokencontext';
 
 const axiosJWT = axios.create({ baseURL: 'http://localhost:3001', withCredentials: true });
 
+// custom hook to return an axios instance with interceptors added for jwt handling
 const useAxiosJWT = () => {
     const { accessToken, setAccessToken } = useContext(TokenContext);
     
@@ -11,11 +12,8 @@ const useAxiosJWT = () => {
         const requestInterceptor = axiosJWT.interceptors.request.use(config => {
             if(!config.headers['Authorization']) {
                 config.headers['Authorization'] = `Bearer ${accessToken}`;
-
             }
             return config;
-
-
         }, error => Promise.reject(error));
 
         const responseInterceptor = axiosJWT.interceptors.response.use(response => response,
@@ -27,16 +25,14 @@ const useAxiosJWT = () => {
                     error.config.headers['Authorization'] = `Bearer ${accessToken}`;
                     return axiosJWT(error.config);
                 }
-                
                 setAccessToken('');
                 return Promise.reject(error);
-
             });
         return () => {
             axiosJWT.interceptors.request.eject(requestInterceptor);
             axiosJWT.interceptors.response.eject(responseInterceptor);
         }
-    }, [accessToken, setAccessToken]);
+    }, [accessToken]);
     return axiosJWT;
 
 }
