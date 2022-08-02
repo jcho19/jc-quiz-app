4b310e686104aa5ed5c6e8b983251abfe416d7af
jcho19 <josephcho212@gmail.com>
2022-08-02 16:27:56 -0700

    revised login component #4

--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -12,6 +12,7 @@ import {
   Box,
   Typography,
   Container,
+  CircularProgress
 } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link as RouterLink } from 'react-router-dom';
@@ -36,7 +37,7 @@ const theme = createTheme({
         fontSize: 24
 
       },
-      '@media (max-width: 700px)' : {
+      '@media (max-width: 480px)' : {
         fontSize: 16
 
       },
@@ -46,7 +47,7 @@ const theme = createTheme({
         fontSize: 20
   
       },
-      '@media (max-width: 700px)' : {
+      '@media (max-width: 480px)' : {
         fontSize: 14
   
       }, 
@@ -58,19 +59,41 @@ const theme = createTheme({
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
-  const { setAccessToken } = useContext(TokenContext);
+  const [errorMsg, setErrorMsg] = useState(''); // message to show user if login wasn't successful
+  const { accessToken, setAccessToken } = useContext(TokenContext);
+  const [isLoading , setIsLoading] = useState(false);
+  
+  // persistent login 
+  useEffect(() => {
+    let isMounted = true;
+    const refresh = async () => {
+      try {
+        setIsLoading(true);
+        const response = await instance.get('/refresh'); // gets access token if refresh token hasn't expired
+        if (isMounted) {
+          setAccessToken(response.data.accessToken);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+      setIsLoading(false);
+    }
+    refresh();
+    
+    return () => isMounted = false;
+  }, []);
+
+  useEffect(() => {
+    setErrorMsg(''); // erase error message if username or password has changed
+  },[username, password])  
+
   const handleSubmit = async e => {
     e.preventDefault();
     try {
+      setIsLoading(true);
       const response = await instance.post('/login', { username, password });
-      console.log(response);
-      setLoggedIn(true);
       setAccessToken(response.data.accessToken);
-  
     } catch (err) {
-      console.log(err);
       if(err.response.status === 401){
         setErrorMsg('Incorrect username or password');
       }
@@ -78,94 +101,100 @@ const Login = () => {
         alert('Login has failed');
       }
     }
-  
+    setIsLoading(false);
   }
-  useEffect(() => {
-    setErrorMsg('');
-  },[username, password])
 
   return (
     <>
-    {loggedIn
-      ? (<ThemeProvider theme={theme}>
-          <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            mt: 8,                          
-          }}>
-            <Typography color='primary.main' variant='h4'>
-              You have succesfully logged in!
-            </Typography>
-            <Link component={RouterLink} to='/' variant='h5'>
-              Let's go back to the home page
-            </Link>
-          </Box>
-
-      </ThemeProvider>)
-      :             
-        (<ThemeProvider theme={theme}>
-          <Container component='main' maxWidth='xs'>
-            <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Avatar sx={{ m: 1, bgcolor: 'primary.main', color: 'secondary.main' }}>
-              </Avatar>
-              <Typography color='primary.main' component='h1' variant='h5'>
-                Login
+    {isLoading
+      ? (<Box sx={{
+        display:'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        mt: 15,
+        }}>
+      <CircularProgress />
+     </Box>)
+      : accessToken
+        ? (<ThemeProvider theme={theme}>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              mt: 8,                          
+            }}>
+              <Typography color='primary.main' variant='h4'>
+                You're logged in!
               </Typography>
-              <Box component='form' noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
-                <TextField
-                  margin='normal'
-                  required
-                  fullWidth
-                  id='username'
-                  label='Username'
-                  name='username'
-                  autoComplete='off'
-                  autoFocus
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-                <TextField
-                  margin='normal'
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                  autoComplete='current-password'
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button
-                  type='submit'
-                  fullWidth
-                  variant='contained'
-                  sx={{ mt: 3, mb: 2, color: 'secondary.main' }}
-                >
-                  Login
-                </Button>
-                <Grid container>
-                  <Grid item>
-                    <Typography color='red' variant='body2'>{errorMsg}</Typography>
-                  </Grid>
-                  <Grid item>
-                    <Link component={RouterLink} to='/signup' variant='body2'>
-                      Don't have an account? Sign Up
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Box>
+              <Link component={RouterLink} to='/' variant='h5'>
+                Let's go back to the home page
+              </Link>
             </Box>
-          </Container>
-        </ThemeProvider>)}
+
+        </ThemeProvider>)
+        :             
+          (<ThemeProvider theme={theme}>
+            <Container component='main' maxWidth='xs'>
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: 'primary.main', color: 'secondary.main' }}>
+                </Avatar>
+                <Typography color='primary.main' component='h1' variant='h5'>
+                  Login
+                </Typography>
+                <Box component='form' noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
+                  <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='username'
+                    label='Username'
+                    name='username'
+                    autoComplete='off'
+                    autoFocus
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    name='password'
+                    label='Password'
+                    type='password'
+                    id='password'
+                    autoComplete='current-password'
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    type='submit'
+                    fullWidth
+                    variant='contained'
+                    sx={{ mt: 3, mb: 2, color: 'secondary.main' }}
+                  >
+                    Login
+                  </Button>
+                  <Grid container>
+                    <Grid item>
+                      <Typography color='red' variant='body2'>{errorMsg}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Link component={RouterLink} to='/signup' variant='body2'>
+                        Don't have an account? Sign Up
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Container>
+          </ThemeProvider>)}
     </>
   );
 }
