6cd575fc6467a55427e20000b365d45a0212cf85
jcho19 <josephcho212@gmail.com>
2022-08-02 23:32:22 -0700

    revised rankings.js in controllers #2

--- server/controllers/rankings.js
+++ server/controllers/rankings.js
@@ -2,9 +2,9 @@ const User = require('../models/user');
 
 const handleRankings = async(req, res) => {
     try {
-        const topUsers = await User.find().sort({ highestScore: -1}).limit(5).exec(); // get the 5 users with the highest scores
+        const topUsers = await User.find().sort({ highestScore: -1 }).limit(5).exec(); // get the 5 users with the highest scores
         // remove unnecssary properties
-        const revisedUsers = topUsers.map(user => ({username: user.username, highestScore: user.highestScore}));
+        const revisedUsers = topUsers.map(user => ({ username: user.username, highestScore: user.highestScore }));
         res.status(200).json(revisedUsers);
     } catch (err) {
         res.status(500).json({ 'message': err.message });
